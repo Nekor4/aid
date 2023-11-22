10e87811eef4c54618cdd9bb3fb34d15f434ef0e
Nekor <konrad.szymanski96@gmail.com>
2023-11-22 18:19:40 +0100

    - added serializable component factory

---
+++ Code/Factory/Serializable/SerializableComponentFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Aid.Factory.Serializable
+{
+    [Serializable]
+    public abstract class SerializableComponentFactory<T> : IFactory<T> where T : Component
+    {
+        [SerializeField] private T prefab;
+        public T Create()
+        {
+            return Object.Instantiate(prefab);
+        }
+    }
+}
