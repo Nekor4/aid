25938f27a0d0142318ca065bf282d23514b4bc64
Nekor <konrad.szymanski96@gmail.com>
2024-03-31 23:05:45 +0200

    - added trail renderer pool

---
+++ Code/Pool/Components/TrailRendererPoolSO.cs
@@ -0,0 +1,13 @@
+﻿using Aid.Factory;
+using Aid.Factory.Serializable;
+using UnityEngine;
+
+namespace Aid.Pool.Components
+{
+    [CreateAssetMenu(menuName = "Aid/Pool/Trail Renderer Pool")]
+    public class TrailRendererPoolSO : ComponentPoolSO<TrailRenderer>
+    {
+        [SerializeField] private SerializableComponentFactory<TrailRenderer> factory;
+        public override IFactory<TrailRenderer> Factory => factory;
+    }
+}
