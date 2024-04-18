4b17138fe5cbdbb654d6c5ec0829bde302945a58
Nekor <konrad.szymanski96@gmail.com>
2024-04-18 22:13:50 +0200

    - fixed health bar async bug

---
+++ Code/HealthComponents/HealthBarsWindow.cs
@@ -0,0 +1,17 @@
+﻿using Aid.UI;
+
+namespace Aid.HealthComponents
+{
+    public class HealthBarsWindow : Window
+    {
+        protected override void OnShown()
+        {
+            HealthBarPresentersRegistry.Show();
+        }
+
+        protected override void OnHidden()
+        {
+            HealthBarPresentersRegistry.Hide();
+        }
+    }
+}
