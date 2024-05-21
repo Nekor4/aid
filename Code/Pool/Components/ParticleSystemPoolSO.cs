d1efe673195ffe37637117be70d889579188aa83
Nekor <konrad.szymanski96@gmail.com>
2024-05-21 21:49:51 +0200

    - added particle system pool

---
+++ Code/Pool/Components/ParticleSystemPoolSO.cs
@@ -0,0 +1,20 @@
+﻿using Aid.Factory;
+using Aid.Factory.Serializable;
+using UnityEngine;
+
+namespace Aid.Pool.Components
+{
+    [CreateAssetMenu(menuName = "Aid/Pool/Particle System Pool")]
+    public class ParticleSystemPoolSO : ComponentPoolSO<ParticleSystemPoolItem>
+    {
+        [SerializeField] private SerializableComponentFactory<ParticleSystemPoolItem> factory;
+        public override IFactory<ParticleSystemPoolItem> Factory => factory;
+
+        protected override ParticleSystemPoolItem Create()
+        {
+            var item = base.Create();
+            item.SetOwnerPool(this);
+            return item;
+        }
+    }
+}
