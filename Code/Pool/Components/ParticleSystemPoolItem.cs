69f6d65a9322b7bb25a82b44f6f88c009c6edf9d
Nekor <konrad.szymanski96@gmail.com>
2024-05-21 21:49:51 +0200

    - added particle system pool

---
+++ Code/Pool/Components/ParticleSystemPoolItem.cs
@@ -0,0 +1,49 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Aid.Pool.Components
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class ParticleSystemPoolItem : MonoBehaviour
+    {
+        private ParticleSystem _particleSystem;
+
+        private ParticleSystemPoolSO _ownerPool;
+
+        private void Awake()
+        {
+            _particleSystem = GetComponent<ParticleSystem>();
+        }
+
+        internal void SetOwnerPool(ParticleSystemPoolSO ownerPool)
+        {
+            _ownerPool = ownerPool;
+        }
+
+        public void Play()
+        {
+            _particleSystem.Play();
+        }
+
+        public void Stop()
+        {
+            _particleSystem.Stop();
+        }
+
+        public void Clear()
+        {
+            _particleSystem.Clear();
+        }
+
+        public void Pause()
+        {
+            _particleSystem.Pause();
+        }
+
+        public async void ReturnInTime(float time)
+        {
+            await UniTask.WaitForSeconds(time);
+            _ownerPool.Return(this);
+        }
+    }
+}
