aa8b6f47bd9cbe94789cd2a3a09a16271f110542
Nekor <konrad.szymanski96@gmail.com>
2024-05-21 21:54:23 +0200

    - added animation curve extensions

---
+++ Code/Extensions/AnimationCurveExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace TapNice.Scripts.GameCore.Extensions
+{
+    using UnityEngine;
+
+    public static class AnimationCurveExtensions
+    {
+        public static int[] CurveToValues(this AnimationCurve curve, int count)
+        {
+            var tempValues = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var value = (int)curve.Evaluate(i);
+                tempValues[i] = value;
+            }
+
+            return tempValues;
+        }
+
+        public static AnimationCurve ValuesToCurve(int[] values)
+        {
+            var curve = new AnimationCurve();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                curve.AddKey(i, values[i]);
+            }
+
+            return curve;
+        }
+    }
+}
