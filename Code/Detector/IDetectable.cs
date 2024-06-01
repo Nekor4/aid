2d7cc4a3afec4ca26d0d3aa16a39cae87df442f7
Nekor <konrad.szymanski96@gmail.com>
2024-06-02 01:14:21 +0200

    - added radius to detectable objects

--- Code/Detector/IDetectable.cs
+++ Code/Detector/IDetectable.cs
@@ -5,12 +5,11 @@ namespace Aid.Detector
 {
     public interface IDetectable
     {
-        public GameObject Owner { get; }
-        public Transform transform { get; }
+        public Vector3 Position { get; }
+        public float Radius { get; }
         public GameObject gameObject { get; }
-
+        public Transform transform { get; }
         public bool IsDetectable { get; }
         public event Action<IDetectable> DetectionChanged;
-        public bool CompareTag(string tag);
     }
 }
